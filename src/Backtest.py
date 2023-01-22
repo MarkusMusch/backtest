78061addd68db9c574181d23d1c5dd90767318ad
Markus <markus-musch@web.de>
2023-01-22 21:03:33 +0100

    New backtest report

--- src/Backtest.py
+++ src/Backtest.py
@@ -64,12 +64,14 @@ class Backtest():
         for timeframe in timeframes:
 
             fig, axs = plt.subplots(num_risk_samples*num_lev_samples
-                                    * num_rr_ratios,
+                                    * num_rr_ratios, 2, 
                                     figsize=(15, 5 * num_risk_samples
                                              * num_lev_samples
-                                             * num_rr_ratios))
+                                             * num_rr_ratios),
+                                    gridspec_kw={'width_ratios': [5, 1]})
             fig.suptitle('Equity Curves and Position Sizes of Backtests for '
-                         + 'Market Structure Break Strategy')
+                         + market.strategy_name)
+            fig.subplots_adjust(hspace=0.4)
 
             for idxr, risk in enumerate(risk_samples):
 
@@ -100,32 +102,38 @@ class Backtest():
                         mean_percentile = np.percentile(sample, 50, axis=1)
                         upper_percentile = np.percentile(sample, 95, axis=1)
                         lower_percentile = np.percentile(sample, 5, axis=1)
-                        axs[idx_subplot].plot(sample, color='y', alpha=0.05)
-                        axs[idx_subplot].plot(mean_percentile, color='r')
-                        axs[idx_subplot].plot(upper_percentile, color='r')
-                        axs[idx_subplot].plot(lower_percentile, color='r')
-                        axs[idx_subplot].plot(rtit.equity_curve, color='b')
+                        axs[idx_subplot][0].plot(sample, color='y', alpha=0.05)
+                        axs[idx_subplot][0].plot(mean_percentile, color='r')
+                        axs[idx_subplot][0].plot(upper_percentile, color='r')
+                        axs[idx_subplot][0].plot(lower_percentile, color='r')
+                        axs[idx_subplot][0].plot(rtit.equity_curve, color='b')
 
-                        axs[idx_subplot].set_ylim([0, 1.1*upper_percentile.max()])
+                        axs[idx_subplot][1].axis('off')
+                        table = axs[idx_subplot][1].table(cellText=[[stats_dict['sharpe']],
+                                                         [stats_dict['sortino']],
+                                                         [stats_dict['max_dd']],
+                                                         [stats_dict['musch']]],
+                                               rowLabels=['Sharpe', 'Sortino',
+                                                          'Max. Drawdown',
+                                                          'Musch'],
+                                               colLabels=['Metrics'], loc='best')
+                        table.auto_set_column_width(col=[0, 1])
 
-                        axs[idx_subplot].set_title(
+                        axs[idx_subplot][0].set_ylim([0, 1.1*upper_percentile.max()])
+
+                        axs[idx_subplot][0].set_title(
                             'Equity Curve ' + market_name
                             + ' at max. risk {:.2%} '.format(risk)
                             + 'with max. leverage {:.1f} '.format(leverage)
-                            + 'and R/R {:.1f} \n'.format(rr)
-                            + 'Sharpe: {:.2f}, '.format(stats_dict['sharpe'])
-                            + 'Sortino: {:.2f}, '.format(stats_dict['sortino'])
-                            + 'Max. '
-                            + 'Drawdown: {:.2%} '.format(stats_dict['max_dd'])
-                            + 'Musch: {:.2f}'.format(stats_dict['musch']))
-                        axs[idx_subplot].grid(True)
+                            + 'and R/R {:.1f} \n'.format(rr))
+                        axs[idx_subplot][0].grid(True)
 
             path = './src/backtests/backtest_reports/' + market.strategy_name \
                    + '/' + market_name + '/'
             if not os.path.exists(path):
                 os.makedirs(path)
 
-            plt.savefig(path + timeframe.value + '.pdf', dpi=75)
+            plt.savefig(path + timeframe.value + '.pdf', dpi=300)
         print(market_name, ' done! \n')
         ms.structure
 
